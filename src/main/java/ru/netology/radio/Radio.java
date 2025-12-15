1bdc76105393547564027b69a5c4d5005fbd0b06
Ksenia Krestsova <arhaatuana@gmail.com>
2025-12-16 00:27:33 +0200

    initial commit

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,57 @@
+package ru.netology.radio;
+
+public class Radio {
+
+    private int radioWave;
+    private int radioVolume;
+
+    public int getCurrentRadioWave() {
+        return radioWave;
+    }
+
+    public void setRadioWave(int newRadioWave) {
+        if (newRadioWave < 0) {
+            radioWave = 9;
+
+            return;
+        }
+        if (newRadioWave > 9) {
+            radioWave = 0;
+
+            return;
+        }
+        radioWave = newRadioWave;
+    }
+
+    public void nextRadioWave() {
+        setRadioWave(radioWave + 1);
+    }
+
+    public void prevRadioWave() {
+        setRadioWave(radioWave - 1);
+    }
+
+    public void setRadioVolume(int newRadioVolume) {
+        if (newRadioVolume < 0) {
+
+            return;
+        }
+        if (newRadioVolume > 100) {
+
+            return;
+        }
+        radioVolume = newRadioVolume;
+    }
+
+    public void increaseRadioVolume() {
+        if (radioVolume < 100) {
+            radioVolume += 1;
+        }
+    }
+
+    public void decreaseRadioVolume() {
+        if (radioVolume > 0) {
+            radioVolume -= 1;
+        }
+    }
+}
